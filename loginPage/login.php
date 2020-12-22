8f990305f93232a3fa78ab9bebdd3634dbee334e
Vansh Chinda <vanshchinda@gmail.com>
2020-12-23 01:49:22 +0530

    test03

--- loginPage/login.php
+++ loginPage/login.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form action="" method="post">
-    <div class="form-element">
-        <label>Username:</label>
-        <input type="text" name="Username" pattern="[a-zA-Z]+" required>
-    </div>
-    <div class="form-element">
-        <label>Password:</label>
-        <input type="password" required>
-    </div>
-    <button type="submit" name="login" value="Login"></button>
+        <div class="form-element">
+            <label>Username:</label>
+            <input type="text" name="Username" pattern="[a-zA-Z]+" required>
+        </div>
+        <div class="form-element">
+            <label>Password:</label>
+            <input type="password" required>
+        </div>
+        <button type="submit" name="login" value="Login"></button>
     </form>
 </body>
+
 </html>
