e64f0b0136c9443291e941d9bd9f20594fe53061
Vansh Chinda <vanshchinda@gmail.com>
2020-12-23 18:05:48 +0530

    Create registration.php

---
+++ classes/registration.php
@@ -0,0 +1,92 @@
+<?php
+
+class Registration
+{
+    /**
+     * @var object database connection
+     */
+    private $db_connection = null;
+    /**
+     * @var array collection of error messages
+     */
+    public $errors = array();
+    /**
+     * @var array  collection of other messages
+     */
+    public $messages = array();
+
+    public function __construct()
+    {
+        if (isset($_POST["register"])) {
+            $this->registerNewUser();
+        }
+    }
+
+    private function registerNewUser()
+    {
+        if (empty($_POST['user_name'])) {
+            $this->errors[] = "Empty Username";
+        } elseif (empty($_POST['user_password_new']) || empty($_POST['user_password_repeat'])) {
+            $this->errors[] = "Empty Password";
+        } elseif ($_POST['user_password_new'] !== $_POST['user_password_repeat']) {
+            $this->errors[] = "Password and password repeat are not the same";
+        } elseif (strlen($_POST['user_password_new']) < 6) {
+            $this->errors[] = "Password has a minimum length of 6 characters";
+        } elseif (strlen($_POST['user_name']) > 64 || strlen($_POST['user_name']) < 2) {
+            $this->errors[] = "Username cannot be shorter than 2 or longer than 64 characters";
+        } elseif (!preg_match('/^[a-z\d]{2,64}$/i', $_POST['user_name'])) {
+            $this->errors[] = "Username does not fit the name scheme: only a-Z and numbers are allowed, 2 to 64 characters";
+        }elseif (!empty($_POST['user_name'])
+            && strlen($_POST['user_name']) <= 64
+            && strlen($_POST['user_name']) >= 2
+            && preg_match('/^[a-z\d]{2,64}$/i', $_POST['user_name'])
+            && !empty($_POST['user_password_new'])
+            && !empty($_POST['user_password_repeat'])
+            && ($_POST['user_password_new'] === $_POST['user_password_repeat'])
+        ) {
+            // creating a database connection
+            $this->db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+            // change character set to utf8 and check it
+            if (!$this->db_connection->set_charset("utf8")) {
+                $this->errors[] = $this->db_connection->error;
+            }
+
+            // if no connection errors
+            if (!$this->db_connection->connect_errno) {
+
+                // escaping, additionally removing everything that could be (html/javascript-) code
+                $user_name = $this->db_connection->real_escape_string(strip_tags($_POST['user_name'], ENT_QUOTES));
+
+                $user_password = $_POST['user_password_new'];
+
+                // crypt the user's password with password_hash() function
+                $user_password_hash = password_hash($user_password, PASSWORD_DEFAULT);
+
+                // check if user name already exists
+                $sql = "SELECT * FROM 'users data'";
+                $query_check_user_name = $this->db_connection->query($sql);
+
+                if ($query_check_user_name->num_rows == 1) {
+                    $this->errors[] = "Sorry, that username is already taken.";
+                } else {
+                    // write new user's data into database
+                    $sql = "INSERT INTO 'users data' (user_name, user_password_hash)
+                            VALUES('" . $user_name . "', '" . $user_password_hash . "');";
+                    $query_new_user_insert = $this->db_connection->query($sql);
+
+                    // if user has been added successfully
+                    if ($query_new_user_insert) {
+                        $this->messages[] = "Your account has been created successfully. You can now log in.";
+                    } else {
+                        $this->errors[] = "Sorry, your registration failed. Please go back and try again.";
+                    }
+                }
+            } else {
+                $this->errors[] = "Sorry, no database connection.";
+            }
+        } else {
+            $this->errors[] = "An unknown error occurred.";
+        }
+    }
+}
